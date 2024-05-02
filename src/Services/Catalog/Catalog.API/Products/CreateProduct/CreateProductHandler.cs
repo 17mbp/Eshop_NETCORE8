95f888e7bd2bb6072c98bd079db8ff15b8df2e90
17mbp <17mbp@users.noreply.github.com>
2024-05-02 16:20:24 -0600

    video 49 udemy .net 8 ddd, cqrs, vertial architec

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+public record CreatedProductCommand(string name, List<string> Category, string Description, string ImageFile, decimal Price)
+    : IRequest<CreatedProductResult>;
+public record CreatedProductResult(Guid Id);
+
+internal class CreatedProductCommandHandler : IRequestHandler<CreatedProductCommand, CreatedProductResult>
+{
+    public Task<CreatedProductResult> Handle(CreatedProductCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -0,0 +1,8 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// ADD SERVICES CONTAINER
+var app = builder.Build();
+
+// HTTP REQUEST
+
+app.Run();
