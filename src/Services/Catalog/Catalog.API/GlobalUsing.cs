718f561405d6a7781dfce02a3848652680bd37fc
17mbp <17mbp@users.noreply.github.com>
2024-05-07 18:31:20 -0600

    no jala en vs

--- src/Services/Catalog/Catalog.API/GlobalUsing.cs
+++ src/Services/Catalog/Catalog.API/GlobalUsing.cs
@@ -1,7 +1,7 @@
-﻿namespace Catalog.API
-{
-    public class GlobalUsing
-    {
-
-    }
-}
+﻿global using Carter;
+global using Marten;
+global using Mapster;
+global using MediatR;
+global using BuildingBlocks.CQRS;
+global using Catalog.API.Models;
+global using Catalog.API.Exceptions;
--- src/Services/Catalog/Catalog.API/obj/Debug/net8.0/Catalog.API.AssemblyInfo.cs
+++ src/Services/Catalog/Catalog.API/obj/Debug/net8.0/Catalog.API.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Catalog.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95f888e7bd2bb6072c98bd079db8ff15b8df2e90")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4113ca48aeb3a960501e4c170e04bc43a9aaa40")]
 [assembly: System.Reflection.AssemblyProductAttribute("Catalog.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Catalog.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
